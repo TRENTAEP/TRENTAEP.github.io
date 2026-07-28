27c5659fc828ea5a2865fa179dcd1d493239d57d
TRENTAEP <hzhzbxbdjsjjsjsk@gmail.com>
2026-07-28 17:42:14 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
 const firebaseConfig = {
@@ -19,3 +24,21 @@ export const auth = getAuth(app);
 signInAnonymously(auth)
   .then(() => console.log("Logged in"))
   .catch(console.error);
+auth.onAuthStateChanged(async (user) => {
+
+  if (!user) return;
+
+  const userRef = doc(db, "users", user.uid);
+
+  const snap = await getDoc(userRef);
+
+  if (!snap.exists()) {
+
+    await setDoc(userRef, {
+      points: 0,
+      created: Date.now()
+    });
+
+  }
+
+});
