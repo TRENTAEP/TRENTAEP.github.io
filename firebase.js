3ed539624a8232e9ff57fab4deeb22a243fa5b9f
TRENTAEP <hzhzbxbdjsjjsjsk@gmail.com>
2026-07-28 17:37:15 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
@@ -9,8 +8,7 @@ const firebaseConfig = {
   projectId: "trentaep-b22a1",
   storageBucket: "trentaep-b22a1.firebasestorage.app",
   messagingSenderId: "714801056227",
-  appId: "1:714801056227:web:f2941326bcd18312aaf8d6",
-  measurementId: "G-ZS4B0Z9RF9"
+  appId: "1:714801056227:web:f2941326bcd18312aaf8d6"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,9 +17,5 @@ export const db = getFirestore(app);
 export const auth = getAuth(app);
 
 signInAnonymously(auth)
-.then(() => {
-    console.log("Logged in");
-})
-.catch((error)=>{
-    console.log(error);
-});
+  .then(() => console.log("Logged in"))
+  .catch(console.error);
